13b39aff2634dbe6bc34eff8716b899a4bab7992
MilesJoseph <miles.klingenberg@outlook.com>
2021-07-11 11:27:30 -0700

    new

--- dags/dag_cluster.py
+++ dags/dag_cluster.py
@@ -21,7 +21,7 @@ SPARK_STEPS = [
                 "spark-submit",
                 "--deploy-mode",
                 "client",
-                "s3://{{ params.BUCKET_NAME }}/params.city.py",
+                "s3://{{ params.BUCKET_NAME }}/city.py",
             ],
         },
     },
