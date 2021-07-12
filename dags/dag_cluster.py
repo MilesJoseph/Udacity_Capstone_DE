88128b68a950b2c96167489df81be8dd7ef88108
MilesJoseph <miles.klingenberg@outlook.com>
2021-07-11 21:54:28 -0700

    new

--- dags/dag_cluster.py
+++ dags/dag_cluster.py
@@ -37,45 +37,43 @@ SPARK_STEPS = [
     }
 ]
 
-
 JOB_FLOW_OVERRIDES = {
-    'Name': 'demo-cluster-airflow',
-    'ReleaseLabel': 'emr-6.2.0',
-    'Applications': [
+    "Name": "Movie review classifier",
+    "ReleaseLabel": "emr-5.29.0",
+    "Applications": [{"Name": "Hadoop"}, {"Name": "Spark"}], # We want our EMR cluster to have HDFS and Spark
+    "Configurations": [
         {
-            'Name': 'Spark'
-        },
-    ],
-    'Instances': {
-        'InstanceGroups': [
-            {
-                'Name': 'Master nodes',
-                'Market': 'ON_DEMAND',
-                'InstanceRole': 'MASTER',
-                'InstanceType': 'm5.xlarge',
-                'InstanceCount': 1,
-            }
-        ],
-        'KeepJobFlowAliveWhenNoSteps': False,
-        'TerminationProtected': False,
-    },
-    'VisibleToAllUsers': True,
-    'JobFlowRole': 'EMR_EC2_DefaultRole',
-    'ServiceRole': 'EMR_DefaultRole',
-    'Tags': [
-        {
-            'Key': 'Environment',
-            'Value': 'Development'
-        },
-        {
-            'Key': 'Name',
-            'Value': 'Airflow EMR Demo Project'
-        },
-        {
-            'Key': 'Owner',
-            'Value': 'Data Analytics Team'
+            "Classification": "spark-env",
+            "Configurations": [
+                {
+                    "Classification": "export",
+                    "Properties": {"PYSPARK_PYTHON": "/usr/bin/python3"}, # by default EMR uses py2, change it to py3
+                }
+            ],
         }
-    ]
+    ],
+    "Instances": {
+        "InstanceGroups": [
+            {
+                "Name": "Master node",
+                "Market": "SPOT",
+                "InstanceRole": "MASTER",
+                "InstanceType": "m4.xlarge",
+                "InstanceCount": 1,
+            },
+            {
+                "Name": "Core - 2",
+                "Market": "SPOT", # Spot instances are a "use as available" instances
+                "InstanceRole": "CORE",
+                "InstanceType": "m4.xlarge",
+                "InstanceCount": 2,
+            },
+        ],
+        "KeepJobFlowAliveWhenNoSteps": True,
+        "TerminationProtected": False, # this lets us programmatically terminate the cluster
+         },
+    "JobFlowRole": "EMR_EC2_DefaultRole",
+    "ServiceRole": "EMR_DefaultRole",
 }
 
 default_args = {
--- dags/transform/city.py
+++ dags/transform/city.py
@@ -4,28 +4,38 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import udf
 Logger= spark._jvm.org.apache.log4j.Logger
 mylogger = Logger.getLogger(__name__)
+from pyspark.sql import SparkSession
 
 def parse_state(x):
     return x.strip().split('-')[-1]
 udf_parse_state = udf(lambda x: parse_state(x), StringType())
 
 
+def main():
+
+    spark = SparkSession \
+        .builder \
+        .appName("transforms") \
+        .getOrCreate()
 #
-demo = spark.read.format('csv').load('s3://capstone-mk/demographics/us-cities-demographics.csv', header=True, inferSchema=True, sep=';')\
+    demo = spark.read.format('csv').load('s3://capstone-mk/demographics/us-cities-demographics.csv', header=True, inferSchema=True, sep=';')\
                 .select("State Code", "City")\
                 .withColumnRenamed("State Code", "state_code")\
                 .withColumnRenamed("City", "city")
 
 #
-us_airport = spark.read.format('csv').load('s3://capstone-mk/codes/airport_code.csv', header=True, inferSchema=True)\
+    us_airport = spark.read.format('csv').load('s3://capstone-mk/codes/airport_code.csv', header=True, inferSchema=True)\
                         .filter("iso_country = 'US'")\
                         .withColumn("state", udf_parse_state("iso_region"))\
                         .selectExpr("municipality AS city", "state AS state_code")
 
 #
-city = us_airport.union(demo)\
+    city = us_airport.union(demo)\
                  .drop_duplicates()\
                  .withColumn("city_id", F.monotonically_increasing_id())
 
 #
-city.write.mode("overwrite").parquet("s3://capstone-mk/lake/city/")
+    city.write.mode("overwrite").parquet("s3://capstone-mk/lake/city/")
+
+if __name__=="__main__":
+    main()
